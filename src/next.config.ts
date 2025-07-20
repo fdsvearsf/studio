f6cc9f83ebacf1aade01e7aee29a684f6908ce12
Warisblogger <warisblogger1@gmail.com>
2025-07-20 07:47:05 +0000

    abhi jo mai new domain ka image link add karta hon woa live add nahi hot

--- src/next.config.ts
+++ src/next.config.ts
@@ -12,51 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.anupsagar.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'apktek.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.ghauseditz.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'bjndyu4zavci6r2z.public.blob.vercel-storage.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pub-13b4c27dd26d40419b31dd08a01050b4.r2.dev',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'rajaneditz.com',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
