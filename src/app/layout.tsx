0eb2fa6c9fccb7bd8c759aa6b6d51bf5e87a810d
Warisblogger <warisblogger1@gmail.com>
2025-07-26 10:09:32 +0000

    light mode cosea all remove plase only dark mod kardi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
-            enableSystem
+            defaultTheme="dark"
+            forcedTheme="dark"
             disableTransitionOnChange
         >
           {children}
